34d6cef9af86e3226f206951ca4bb08e53c0ffc5
kunj-21 <107642217+kunj-21@users.noreply.github.com>
2023-12-12 17:25:34 +0530

    Add files via upload

---
+++ src/components/CardsData.js
@@ -0,0 +1,149 @@
+const BookData=[
+    {
+        id: 1,
+        rname: "Words of Radiance",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1659905768i/17332218._SY75_.jpg",
+        address: " Brandon Sanderson",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "total score 15,754",
+        price: 350,
+        rating: "4.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "Harry Potter and the Deathly Hallows",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1663805647i/136251._SY75_.jpg",
+        address: " J.K. Rowling",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "  total score 11,754",
+        price: 380,
+        rating: "4.4",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "Crooked Kingdom",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1456172607i/22299763._SY75_.jpg",
+        address: " Leigh Bardugo",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "total score 9,051",
+        price: 280,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "A Court of Mist and Fury ",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1620325671i/50659468._SY75_.jpg",
+        address: " Sarah J. Maas ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 6,082",
+        price: 300,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "The Return of the King",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1654216226i/61215384._SY75_.jpg",
+        address: " J.R.R. Tolkien",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "total score 6,981",
+        price: 220,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "Fourth Wing",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1701980900i/61431922._SY75_.jpg",
+        address: "Rebecca Yarros",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 6,760",
+        price: 315,
+        rating: "4.1",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "The House of Hades",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1464201430i/12127810._SY75_.jpg",
+        address: " Rick Riordan",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "total score 7,980",
+        price: 350,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "The Complete Maus",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327354180i/15195._SX50_.jpg",
+        address: " Art Spiegelman",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 3,303",
+        price: 330,
+        rating: "3.6",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "The Nightingale",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1681839850i/21853621._SY75_.jpg",
+        address: " Kristin Hannah",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 3,287",
+        price: 230,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "Know My Name",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1567612158l/50196744._SX50_SY75_.jpg",
+        address: "Chanel Miller",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 3,064",
+        price: 280,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "heartstopper: Volume Four",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1640745736i/56060300._SX50_.jpg",
+        address: "Alice Oseman",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 3,037",
+        price: 300,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 1,
+        rname: "The Wise Man's Fear",
+        imgdata: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1452624392i/1215032._SY75_.jpg",
+        address: "Patrick Rothfuss",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " total score 2,949",
+        price: 340,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+]
+
+
+export default BookData;
